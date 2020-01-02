405038b17a15ccd9c7c825a7028894d394782994
Akshay <itsakshay95@gmail.com>
2020-01-02 14:32:56 +0530

    code clean

--- flask-app/api.py
+++ flask-app/api.py
@@ -2,18 +2,18 @@ from flask import Flask,jsonify
 from primeHelper import rightTruPrime,leftTruPrime
 app = Flask(__name__) 
   
-@app.route('/hello/<name>') 
-def hello_name(name): 
-   return 'Hello %s!' % name 
+@app.route('/api', methods = ['GET']) 
+def apiWelcome(): 
+   return 'Welcome to Flask API demo : Two sided prime number.'
 
-@app.route('/home/<int:num>', methods = ['GET']) 
-def disp(num): 
+@app.route('/api/<int:num>', methods = ['GET']) 
+def isTwoSidedPrime(num): 
     if(rightTruPrime(num) & leftTruPrime(num)):
         result='true'
     else:
         result='false'
 
-    return jsonify({'Is two sided prime ': result}) 
+    return jsonify({str(num) + ' is two sided prime? ': result}) 
   
 if __name__ == '__main__': 
    app.run()   
